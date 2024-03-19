b17a6475b8c538b76992d2a478a639bd5eb0f0c8
Marcos Anjos <marcosbanjos@gmail.com>
2024-03-19 17:21:39 -0300

    Primeiro commit em java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        int c = 2;
+        int d;
+
+        d = a + b + c;
+    }
+
+
+
+}
